336de57b5b1522086718b666e4c190ff4220562a
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-02 15:48:01 -0300

    :sparkles: feat: Adds entity and id markers in User class.

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,9 +1,14 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String email;
