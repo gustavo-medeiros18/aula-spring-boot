8fedc32374e4648a5cf5bb982c0cb867b8e0e9a1
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-03 11:52:20 -0300

    :sparkles: feat: Implements findAll() and findById() methods in UserService.

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,5 +1,24 @@
 package com.educandoweb.course.services;
 
-public class UserService {
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+  @Autowired
+  private UserRepository repository;
+
+  public List<User> findAll() {
+    return repository.findAll();
+  }
+
+  public User findById(Long id) {
+    Optional<User> user = repository.findById(id);
+    return user.get();
+  }
 }
