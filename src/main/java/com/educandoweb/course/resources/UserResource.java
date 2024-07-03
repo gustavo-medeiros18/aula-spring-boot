6dcbc1041a568d60af86fbc61c1b07d6dd1c8f38
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-03 11:53:48 -0300

    :sparkles: feat: Adds UserService dependency usage in UserResource.findAll.

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,24 +1,28 @@
 package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+  @Autowired
+  private UserService service;
 
   @GetMapping
-  public ResponseEntity<User> findAll() {
-    User u = new User(
-        1L,
-        "Maria",
-        "maria@email.com",
-        "99999999",
-        "12345"
-    );
+  public ResponseEntity<List<User>> findAll() {
+    List<User> users = service.findAll();
+
+    return ResponseEntity.ok().body(users);
+  }
 
     return ResponseEntity.ok().body(u);
   }
