df1a9b394fc6bbaefdc406fe2873300c6ebc8254
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-03 15:41:11 -0300

    :sparkles: feat: Adds order seeding on TestConfig.run() method.

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.config;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 /**
@@ -34,6 +37,9 @@ public class TestConfig implements CommandLineRunner {
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private OrderRepository orderRepository;
+
   @Override
   public void run(String... args) throws Exception {
     User u1 = new User(
@@ -52,5 +58,11 @@ public class TestConfig implements CommandLineRunner {
     );
 
     userRepository.saveAll(Arrays.asList(u1, u2));
+
+    Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+    Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+    Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
+    orderRepository.saveAll(Arrays.asList(o1, o2, o3));
   }
 }
