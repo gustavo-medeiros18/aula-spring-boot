aa033bd836ea68cbe8cf08e97524f5088ee7ed7c
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-03 15:37:34 -0300

    :sparkles: feat: Adds OrderResource controller with and findAll() and findById() routes.

---
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -0,0 +1,32 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+  @Autowired
+  private OrderService orderService;
+
+  @GetMapping
+  public ResponseEntity<List<Order>> findAll() {
+    List<Order> orders = orderService.findAll();
+
+    return ResponseEntity.ok().body(orders);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Order> findById(Long id) {
+    Order order = orderService.findById(id);
+
+    return ResponseEntity.ok().body(order);
+  }
+}
