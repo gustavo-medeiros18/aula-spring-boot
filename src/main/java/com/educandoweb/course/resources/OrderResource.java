f36c9509363e0381d19bc23b43999c111b2443d9
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-04 11:36:06 -0300

    :bug: fix: Adds @PathVariable annotation to OrderResource.findById() method.

--- src/main/java/com/educandoweb/course/resources/OrderResource.java
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -5,6 +5,7 @@ import com.educandoweb.course.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class OrderResource {
   }
 
   @GetMapping(value = "/{id}")
-  public ResponseEntity<Order> findById(Long id) {
+  public ResponseEntity<Order> findById(@PathVariable Long id) {
     Order order = orderService.findById(id);
 
     return ResponseEntity.ok().body(order);
