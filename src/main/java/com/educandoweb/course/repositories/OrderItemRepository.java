5fc21ec157acce56a9c955a26364c5ceec783b13
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-05 09:45:00 -0300

    :sparkles: feat: Adds OrderItemRepository interface.

---
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -0,0 +1,7 @@
+package com.educandoweb.course.repositories;
+
+import com.educandoweb.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+}
