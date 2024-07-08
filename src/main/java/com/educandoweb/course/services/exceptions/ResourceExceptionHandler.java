5e3a092bc5298ca97b5998f3a0e4084db77975e9
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-07-08 09:33:21 -0300

    :sparkles: feat: Adds ResourceExceptionHandler class.

---
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.educandoweb.course.services.exceptions;
+
+import com.educandoweb.course.resources.exceptions.StandardError;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * ControllerAdvice annotation allows you to handle
+ * exceptions across the whole application, not just
+ * for a specific controller.
+ * <p>
+ * HttpServletRequest is an interface that defines methods
+ * to provide client request information to a servlet.
+ */
+@ControllerAdvice
+public class ResourceExceptionHandler {
+  /**
+   * ExceptionHandler annotation allows you to handle
+   * specific exceptions in specific methods.
+   */
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<StandardError> resourceNotFoundException(
+      ResourceNotFoundException e,
+      HttpServletRequest request
+  ) {
+    String error = "Resource not found";
+    HttpStatus status = HttpStatus.NOT_FOUND;
+
+    StandardError err = new StandardError(
+        System.currentTimeMillis(),
+        status.value(),
+        error,
+        e.getMessage(),
+        request.getRequestURI()
+    );
+
+    return ResponseEntity.status(status).body(err);
+
+  }
+}
